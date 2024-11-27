0026c3439f102d2ab268d4a40e741bf1e692970f
k01756ws <william.shedden@student.manchester.ac.uk>
2024-11-27 19:51:41 +0000

    Show blob

--- src/main/app.js
+++ src/main/app.js
@@ -4,8 +4,8 @@ import { Renderer } from "./renderer.js";
 document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.getElementById("blobCanvas");
     const context = canvas.getContext("2d");
-    canvas.width = 800;
-    canvas.height = 600;
+    canvas.width = 1200;
+    canvas.height = 800;
     const renderer = new Renderer(context);
     renderer.drawBlob();
 });
